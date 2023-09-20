d9ebbf02c42ed025e103bc5069b21c5621f9adb3
Hazlan Muhammad Qodri <byze.eternalground@gmail.com>
2023-09-20 21:05:01 +0700

    add firewall rules

--- 1-example.tf
+++ 1-example.tf
@@ -8,9 +8,23 @@ locals {
 
 provider "google" {
   project = local.project_id
-  region = "asia-southeast2"
+  region  = "asia-southeast2"
 }
 
 resource "google_service_account" "nginx" {
   account_id = "nginx-demo"
+}
+
+resource "google_compute_firewall" "name" {
+  name    = "web-access"
+  network = local.network
+
+  # open port :80 for reverse proxy nginx
+  allow {
+    protocol = "tcp"
+    ports    = ["80"]
+  }
+
+  source_ranges = ["0.0.0.0/0"]
+  target_service_accounts = [google_service_account.nginx.email]
 }
